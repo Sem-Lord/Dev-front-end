bd230641eaab08e6b491242d781550a5e311d544
Sem-Lord <56391743+Sem-Lord@users.noreply.github.com>
2020-01-21 19:22:55 +0100

    Add files via upload

---
+++ Acceuil.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
+    <title>Pricing example ยท Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script type="text/javascript" src="https://ff.kis.scr.kaspersky-labs.com/3D811E7A-FD7B-1141-8895-9D4D0DC03731/main.js" charset="UTF-8"></script><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="pricing.css" rel="stylesheet">
+	<link rel="stylesheet" href="newcss.css" />
+	<link  href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+
+<body>
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+        <img src="l.JPG" id="flotement"/>
+		<h5 class="my-0 mr-md-auto font-weight-normal"></h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+            <a class="p-2 text-dark" href="#">Accueil</a>
+            <a class="p-2 text-dark" href="Recherche.php">Recherche</a>
+            <a class="p-2 text-dark" href="A propos.php">A propos</a>
+        </nav>
+
+        <a class="btn btn-outline-primary" href="Administration.php">Se connecter</a>
+    </div>
+
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<article class="col-2">
+				<h3>SangPartout</h3>
+				<p>Ne paniquer plus pour un membre de votre famille dans besoin de sang.Avec SangPartoout, vous vos amis parents et proches ne risquez plus rien en cas de transfusion sanguine.tout est mis en oeuvre pour garantir votre bon rétablissement.
+				</p>
+				</article>
+				<article class="col-1">
+				<p><img src="affiche.JPG" id="flotter_gauch"></p>
+				</article>
+				
+				
+			</div>
+		</div>
+	</div>
+    <footer class="pt-4 my-md-5 pt-md-5 border-top" class="noir">
+        <div class="row">
+            <div class="col-12 col-md">
+                <img class="mb-2" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+                <small class="d-block mb-3 text-muted text-center">&copy; <b>juillet-2019</b></small>
+            </div>
+        </div>
+    </footer>
+    </div>
+</body>
+
+</html>
